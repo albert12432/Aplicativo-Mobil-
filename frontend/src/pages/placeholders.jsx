6a0e649eed5953bc362219fa6ea16846b67522e1
Samuel Moreno Ramírez <71468965+albert12432@users.noreply.github.com>
2025-11-10 17:23:00 -0500

    Delete frontend directory

--- frontend/src/pages/placeholders.jsx
+++
@@ -1,5 +0,0 @@
-// Placeholders para páginas que serán desarrolladas
-export const SubjectView = () => <div className="p-6"><h1>Vista de Materia</h1></div>
-export const ExamView = () => <div className="p-6"><h1>Vista de Examen</h1></div>
-export const ExamResults = () => <div className="p-6"><h1>Resultados de Examen</h1></div>
-export const Profile = () => <div className="p-6"><h1>Perfil</h1></div>
